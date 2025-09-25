0b34454182100cd55b3d229ca76056e60352ce82
luisffernandesc <a18650@alunos.ipca.pt>
2025-09-25 09:48:56 +0200

    Start Cipher Method

--- e1/src/SubstitutionCipher.java
+++ e1/src/SubstitutionCipher.java
@@ -1,2 +1,26 @@
 public class SubstitutionCipher {
+
+    private static String[] lowerCase =
+            {"a", "b", "c", "d", "e", "f",
+            "g", "h", "i", "j", "k", "l", "m", "n",
+            "o", "p", "q", "r", "s", "t", "u", "v",
+            "w", "x", "y", "z"};
+
+    private static String[] upperCase =
+            {"a", "b", "c", "d", "e", "f",
+            "g", "h", "i", "j", "k", "l", "m", "n",
+            "o", "p", "q", "r", "s", "t", "u", "v",
+            "w", "x", "y", "z"};
+
+    public static String encode(int key, String text){
+
+        return text;
+
+    }
+
+    public static String decode(String key, String text){
+
+        return text;
+    }
+
 }
