155d2e409e3165d8f7d5a7e0eddfd82ddb579682
Veucci <efe.ozkan732@gmail.com>
2025-09-25 10:20:31 +0200

    e2 Placeholder File

---
+++ e2/src/Slopes.java
@@ -0,0 +1,13 @@
+public class Slopes {
+
+    public static String downTheSlope(String map, int i1, int i2) {
+        // Placeholder
+        return "Return Message";
+    }
+
+    public static String jumpTheSlope(String map, int i1, int i2) {
+        // Placeholder
+        return "Return Message";
+    }
+
+}
