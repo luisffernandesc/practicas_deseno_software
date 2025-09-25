908400091ed277ef5a532cd8c65d0982e0e7df94
luisffernandesc <a18650@alunos.ipca.pt>
2025-09-25 09:50:39 +0200

    Amend Upper Case Alphabet

--- e1/src/SubstitutionCipher.java
+++ e1/src/SubstitutionCipher.java
@@ -7,10 +7,9 @@ public class SubstitutionCipher {
             "w", "x", "y", "z"};
 
     private static String[] upperCase =
-            {"a", "b", "c", "d", "e", "f",
-            "g", "h", "i", "j", "k", "l", "m", "n",
-            "o", "p", "q", "r", "s", "t", "u", "v",
-            "w", "x", "y", "z"};
+            {"A", "B", "C", "D", "E", "F", "G", "H", "I",
+            "J", "K", "L", "M", "N", "O", "P", "Q", "R",
+            "S", "T", "U", "V", "W", "X", "Y", "Z"};
 
     public static String encode(int key, String text){
 
